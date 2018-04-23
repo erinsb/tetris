95d00b77290add0b2576fd9122a0f3f2ee0338ef
erinsb <esimonsbrown@gmail.com>
2018-04-22 21:53:44 -0600

    Added overlap checking

--- tetris2/tetris2.ino
+++ tetris2/tetris2.ino
@@ -13,18 +13,37 @@
 
 #define STARTX  12
 #define STARTY  10
-#define LENGTH  20
-#define WIDTH   11
+#define LENGTH  10
+#define WIDTH   9
 
-int shape1[4][4] = {{1,1,0,0}, {1,1,0,0}}; //square
-int shape2[4][4] = {{1,1,1,1}}; //I shape
-int shape3[4][4] = {{1},{1},{1},{0,1}}; //L
-int shape4[4][4] = {{1,1},{0,1},{0,1}}; //Z
-int gameBoard[WIDTH][LENGTH] = {};
+
+byte shape1[4][4] = {
+                    {0,0,0,0},
+                    {0,1,1,0},
+                    {0,1,1,0},
+                    {0,0,0,0}}; //square
+byte shape2[4][4] = {
+                    {0,0,0,0},
+                    {1,1,1,1},
+                    {0,0,0,0},
+                    {0,0,0,0}}; //I shape
+byte shape3[4][4] = {
+                    {0,1,0,0},
+                    {0,1,0,0},
+                    {0,1,1,0},
+                    {0,0,0,0}}; //L
+byte shape4[4][4] = {
+                    {0,0,0,0},
+                    {1,1,0,0},
+                    {0,1,1,0},
+                    {0,0,0,0}}; //Z
+
+//bool gameBoard[WIDTH][LENGTH];
+byte gameBoard[LENGTH+1] = {0,0,0,0,0,0,0,0,0,0,B11111111};
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
-void draw(int shapeMatrix[4][4], int startX, int startY) {
+void draw(byte shapeMatrix[4][4], int startX, int startY) {
   
     for(int i=0; i<4; i++) {
       for(int j=0; j<4; j++) {
@@ -35,7 +54,7 @@ void draw(int shapeMatrix[4][4], int startX, int startY) {
   }; 
 }
 
-void clearShape(int shapeMatrix[4][4], int startX, int startY) {
+void clearShape(byte shapeMatrix[4][4], int startX, int startY) {
 
   for(int i=0; i<4; i++) {
     for(int j=0; j<4; j++) {
@@ -46,46 +65,85 @@ void clearShape(int shapeMatrix[4][4], int startX, int startY) {
   }
 };
 
-void rotate(int shapeMatrix[4][4], int startX, int startY) {
+void rotate(byte shapeMatrix[4][4], int startX, int startY) {
 
-  for(int i=0; i<4; i++) {
-    for(int j=0; j<4; j++) {
-      if(shapeMatrix[i][j] == 1) {
-        matrix.drawPixel(i + startX, j + startY, matrix.Color333(0, 0, 0));
+  if (startX+4 < STARTX + WIDTH and startY+4 < STARTY + LENGTH) {
+//
+//    for(int i=0; i<4; i++) {
+//      for(int j=0; j<4; j++) {
+//        if(shapeMatrix[i][j] == 1) {
+//          matrix.drawPixel(i + startX, j + startY, matrix.Color333(0, 0, 0));
+//        }
+//      }
+//    }
+  
+    // Transpose
+    for (int i=0; i<4; i++) {
+      for (int j=i; j<4; j++) {
+        int temp = shapeMatrix[i][j];
+        shapeMatrix[i][j] = shapeMatrix[j][i];
+        shapeMatrix[j][i] = temp;
+      }
+    };
+  
+    // Reverse columns
+    for (int i=0; i<4; i++) {
+      for (int j=0, k=3; j<k; j++, k--) {
+        int temp = shapeMatrix[j][i];
+        shapeMatrix[j][i] = shapeMatrix[k][i];
+        shapeMatrix[k][i] = temp;
+      }
+    };
+  } else {
+  }
+}
+
+void addShape(byte shape[4][4], int x, int y) {
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<4; j++) {
+      if (shape[i][j] == 1) {
+        matrix.drawPixel(i + x, j + y, matrix.Color333(4, 0, 0));
+       // Serial.println(i+x);
+      byte mask = B10000000;
+       gameBoard[j+y-STARTY] = (mask>>i+x-STARTX | gameBoard[j+y-STARTY]);
+       //Serial.print(gameBoard[j+y]);
+       //Serial.println(j+y-STARTY);
+        //gameBoard[i+x-STARTX][j+y-STARTY] = 1;
+        
       }
     }
   }
+}
 
-  // Transpose
-  for (int i=0; i<4; i++) {
-    for (int j=i; j<4; j++) {
-      int temp = shapeMatrix[i][j];
-      shapeMatrix[i][j] = shapeMatrix[j][i];
-      shapeMatrix[j][i] = temp;
+bool checkBoard(byte shape[4][4], int x, int y) {
+   for (int i=0; i<4; i++) {
+    for (int j=0; j<4; j++) {
+      if (shape[i][j] == 1) {
+        byte mask = B10000000;
+        Serial.print("MASK ");
+        Serial.println(mask>>i+x-STARTX);
+        Serial.println(gameBoard[j+y-STARTY]);
+        Serial.println(j+y-STARTY);
+        Serial.println(gameBoard[j+y-STARTY] & (mask>>i+x-STARTX));
+        if ((gameBoard[j+y-STARTY] | (mask>>i+x-STARTX) == 0) ){
+          return false;
+        }
+      }
     }
-  };
-
-  // Reverse columns
-  for (int i=0; i<4; i++) {
-    for (int j=0, k=3; j<k; j++, k--) {
-      int temp = shapeMatrix[j][i];
-      shapeMatrix[j][i] = shapeMatrix[k][i];
-      shapeMatrix[k][i] = temp;
-    }
-  };
-  
+  }
+  return true;
 }
 
 
 void setup() {
 
-  //Serial.begin(9600);
+  Serial.begin(9600);
 
   matrix.begin();
+
+  matrix.drawLine(STARTX-1,STARTY+LENGTH,STARTX+WIDTH,STARTY+LENGTH, matrix.Color333(7,7,7));
   matrix.drawLine(STARTX-1,STARTY,STARTX-1,STARTY+LENGTH, matrix.Color333(7, 7, 7));
-  matrix.drawLine(STARTX-2,STARTY,STARTX-2,STARTY+LENGTH, matrix.Color333(7, 7, 7));
   matrix.drawLine(STARTX+WIDTH,STARTY,STARTX+WIDTH,STARTY+LENGTH, matrix.Color333(7, 7, 7));
-  matrix.drawLine(STARTX+WIDTH+1,STARTY,STARTX+WIDTH+1,STARTY+LENGTH, matrix.Color333(7, 7, 7));
   
 
 }
@@ -97,17 +155,31 @@ void setup() {
 void loop() {
   int startX = STARTX;
   int startY = STARTY;
+  int lastY = STARTY;
+  int lastX = STARTX;
   // put your main code here, to run repeatedly:
   delay(500);
   //Serial.println("HERE");
-  for (int i=0; i<10; i++) {
-
+  int shape[4][4];
+  
+  while (checkBoard(shape4, startX, startY)) {
+    
     draw(shape4, startX, startY);
     delay(500);
     clearShape(shape4, startX, startY);
+    
     rotate(shape4, startX, startY);
+    delay(500);
+    lastY = startY;
+    lastX = startX;
     startY++;
+    //Serial.println(startY);
   }
+  //Serial.println(gameBoard);
+  addShape(shape4, lastX, lastY);
+  //gameBoard[0] = 1;
+  Serial.println("HERE");
+  
 
 
 
